cd4398477f518a15975b5e5f0fb40d28f812a911
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-25 12:42:07 -0400

    :sparkles: Añadida configuración en archivo "package.json"
    Se agregaron los scripts "start", "dev" y "build" para poder usarlos con npm

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/.babelrc: Error while parsing config - JSON5: invalid end of input at 1:1\\n    at syntaxError (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/json5/lib/parse.js:1083:17)\\n    at invalidEOF (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/json5/lib/parse.js:1032:12)\\n    at Object.start (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/json5/lib/parse.js:821:19)\\n    at Object.parse (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/json5/lib/parse.js:32:32)\\n    at /mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/@babel/core/lib/config/files/configuration.js:264:23\\n    at /mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/@babel/core/lib/config/files/utils.js:30:12\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/@babel/core/lib/gensync-utils/async.js:25:3)\\n    at Generator.next (<anonymous>)\\n    at step (/mnt/f/.Knowledge/Platzi/Desarrollo Web/Webpack-Practico/backend/node_modules/gensync/index.js:261:32)\");\n\n//# sourceURL=webpack://webpackbackendpractico/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
